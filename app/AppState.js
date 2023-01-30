e55e6424560fdb13add1b1ed41f99f030b60b462
Jaden Hawkes <jadenhawkes@gmail.com>
2023-01-30 15:44:45 -0700

    Firing Up

--- app/AppState.js
+++ app/AppState.js
@@ -2,10 +2,11 @@ import { Value } from "./Models/Value.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 import { loadState } from "./Utils/Store.js"
+import { Player } from "./Models/Players.js"
 
 class AppState extends EventEmitter {
-  /** @type {import('./Models/Value').Value[]} */
-  values = loadState('values', [Value])
+  /**@type import('./Models/Players.js').Player[] */
+  Player = [new Player('kait', 2)];
 }
 
 export const appState = new Proxy(new AppState(), {
---
+++ app/Controllers/PlayersController.js
@@ -0,0 +1,6 @@
+import { appState } from "../AppState.js";
+import { Player } from "../Models/Players.js";
+
+constructor() {
+  console.log('beep beep boop building);
+}
